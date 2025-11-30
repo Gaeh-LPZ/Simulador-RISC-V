a47b05b3c9c924c8a6fe702116b63f4b67e6850a
Gaeh-LPZ <galob567@gmail.com>
2025-11-29 22:40:00 -0600

    Made the vite compatibility to use code mirror

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import { EditorState } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import { defaultKeymap } from "@codemirror/commands";
+
+let startState = EditorState.create({
+    doc : "addi x1, x0, 4",
+    keymap : keymap.of(defaultKeymap)
+});
+
+let view = new EditorView ({
+    state : startState,
+    parent : document.getElementById('editor')
+});
